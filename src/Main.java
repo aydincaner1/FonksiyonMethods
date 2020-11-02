f6c9bdf0b1e887bd54cacb05daff0fe8ecb77cd9
Caner Aydın <caner.aydd@gmail.com>
2020-11-02 22:49:59 +0300

    java chapter 1 ipvalidator commit !

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,23 @@
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        CustomerManager customerManager = new CustomerManager();
-        customerManager.Add();
+        //CustomerManager customerManager = new CustomerManager();
+        //customerManager.Add();
 
-        Toplammethod toplammethod = new Toplammethod();
-        toplammethod.topla3();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("IP adresini giriniz:");
+        String ip = scanner.nextLine();
+
+        //String message = Ip4Validator.validate(ip);
+        String message =ipValidator.validate(ip);
+        System.out.println(message);
+
+        scanner.close();
+
+        //Toplammethod toplammethod = new Toplammethod();
+        //toplammethod.topla3();
 
 
 
---
+++ src/SringPoolController.java
@@ -0,0 +1,22 @@
+
+class ipValidator {
+
+    //public void kelimedeneme() {
+        //System.out.println("kelimerler burada calisacak..");}
+    private static String ipPrefix = "11.122.10.";
+
+    public static String validate(String ip) {
+
+        if(!ip.startsWith(ipPrefix)) {
+            return "GeÃ§ersiz IP girildi, IP : 11.122.10 seklinde baslamali ! ";
+        }
+
+        String mesaj = "Devoloper IP: " + ip;
+        String[] ipSegments = ip.split("\\.");
+        String lastSegment = ipSegments[ipSegments.length-1];
+        if(ipSegments.length!= 4) {
+            return "Ip uzunluğu yanlış, ip düzenleyeniz ! ";
+        }
+        return mesaj;
+    }
+}
